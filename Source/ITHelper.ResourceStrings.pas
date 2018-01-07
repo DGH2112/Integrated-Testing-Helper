ed6c8b30d89c30daaa3489ce86821de789d2e8c1
DGH2112 <davidghoyle@gmail.com>
2018-01-07 16:27:15 +0000

    Updated the resource string Updated the resource string to denote that the application is built in a DEBUG mode.

--- Source/ITHelper.ResourceStrings.pas
+++ Source/ITHelper.ResourceStrings.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    05 Jan 2018
+  @Date    07 Jan 2018
   
 **)
 Unit ITHelper.ResourceStrings;
@@ -14,8 +14,13 @@ Interface
 ResourceString
   (** This is a message string to appear in the splash screen **)
   strSplashScreenName = 'Integrated Testing Helper %d.%d%s for %s';
+  {$IFDEF DEBUG}
   (** This is another message string to appear in the splash screen **)
   strSplashScreenBuild = 'Freeware by David Hoyle (Build %d.%d.%d.%d)';
+  {$ELSE}
+  (** This is another message string to appear in the splash screen **)
+  strSplashScreenBuild = 'Freeware by David Hoyle (Build %d.%d.%d.%d)';
+  {$ENDIF}
   (** This is a resourcestring for the message tab name. **)
   strITHelperGroup = 'ITHelper Messages';
 
